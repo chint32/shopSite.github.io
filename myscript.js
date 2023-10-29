a369c0d64da168b1e2fa007f7f8f1dc60f08b615
chint32 <46208209+chint32@users.noreply.github.com>
2023-10-28 22:36:54 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,47 +1,26 @@
-function hello(){
-    alert("Hello!");
+let slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-/*
-let cart = [];
-let cartTotal = 0;
-
-function addToCart(productId) {
-    // In a real application, you would need to implement product addition logic here.
-    // For this example, we'll simply update the cart visually.
-    
-    // cart.push(productId);
-    // cartTotal += getProductPrice(productId);
-
-    if (productId === 1) {
-        cartTotal = 10.00;
-    } else {
-        cartTotal = 15.00;
-    }
-    // Update the cart display
-    updateCartDisplay();
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-function getProductPrice(productId) {
-    // In a real application, you'd fetch the product's price from a database.
-    // For this example, we'll return static prices.
-    if (productId === 1) {
-        return 10.00;
-    } else if (productId === 2) {
-        return 15.00;
-    }
-    return 0.00;
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1; }    
+  if (n < 1) { slideIndex = slides.length; }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
-
-
-function updateCartDisplay() {
-    const cart = document.getElementById("cart");
-    const cartItems = cart.children[0];
-    const cartTotalElement = cart.children[1];
-
-    cartItems.innerHTML = "New Item!";
-    cartTotalElement.textContent = 10.toFixed(2);
-}
-
-*/
 
